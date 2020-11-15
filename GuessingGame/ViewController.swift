34f64de2abcb52b2b28709acfec992f5e204acec
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-14 16:00:26 -0800

    Game Complete

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
+    let label = UILabel()
     
     var countries = [String]()
     var correctAnswer = 0
     var score = 0
+    var questionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +32,10 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
+        label.textColor = UIColor.blue
+        label.text = "Score: 0";
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: label)
+        
         askQuestion()
     }
     
@@ -43,8 +49,16 @@ class ViewController: UIViewController {
         title = countries[correctAnswer].uppercased()
     }
     
+    func startOver(action: UIAlertAction! = nil){
+        correctAnswer = 0
+        score = 0
+        questionsAsked = 0
+        askQuestion()
+    }
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        questionsAsked += 1
         
         if(sender.tag == correctAnswer){
             title = "Correct!"
@@ -54,9 +68,17 @@ class ViewController: UIViewController {
             score -= 1
         }
         
-        let ac = UIAlertController(title: "Answer was \(countries[correctAnswer])", message: "Your score is \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
+        label.text = "Score: \(score)";
+        
+        if(questionsAsked == 10){
+            let finalAC = UIAlertController(title: "Game Complete!", message: "You got \(score) out of 10 questions right", preferredStyle: .alert)
+            finalAC.addAction(UIAlertAction(title: "Play Again", style: .default, handler: startOver))
+            present(finalAC, animated: true)
+        }else{
+            let ac = UIAlertController(title: "Answer was \(countries[correctAnswer])", message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
     }
     
 
